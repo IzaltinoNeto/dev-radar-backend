53077060fddac0f4f3e7b2c3af38956e3b68fa92
IzaltinoNeto <izaltino_neto@hotmail.com>
2020-01-26 11:50:58 -0400

    Add webSocket module

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -2,7 +2,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
-
+const {findConnections, sendMessage} = require('../websocket');
 module.exports = {
     async index(req,res){
         const dev = await Dev.find();
@@ -35,7 +35,17 @@ module.exports = {
             techs : techsArray,
             location
         });
-    
+
+        //Filtrar as Conexões há no máximo 10km de distancia
+        //com pelo menos uma das tecnologias filtradas
+
+        const sendSocketMessageTo = findConnections(
+            { latitude, longitude },
+            techsArray
+            )
+
+           sendMessage(sendSocketMessageTo, 'new-dev', dev);
+            
         }
 
         
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-const express = require('express');
 
+const { setupWebSocket } = require('./websocket');
+const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http')
 const routes = require('./routes');
+
+const server = http.Server(app);
+
+setupWebSocket(server);
 
 mongoose.connect('mongodb+srv://izaltino:13852749@cluster0-rbbc6.mongodb.net/week10?retryWrites=true&w=majority',
 {
@@ -10,12 +17,8 @@ mongoose.connect('mongodb+srv://izaltino:13852749@cluster0-rbbc6.mongodb.net/wee
     useUnifiedTopology: true
 })
 
+app.use(cors());
 app.use(express.json());
-
-
-//Query Params: request.query (Filtros, Ordenação, Paginação..)
-//Route Params: request.params (Identificar um recurso na alteração ou remoção)
-//Body: 
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
